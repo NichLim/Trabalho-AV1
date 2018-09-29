48bed665d9f15e3851dab0942ed47cdf0b093b26
Gabriel Julio <43018141+GabrielJulio@users.noreply.github.com>
2018-09-29 18:53:30 -0300

    Começando professor

---
+++ Trabalho-AV1/src/modelo/Professor.java
@@ -0,0 +1,22 @@
+package modelo;
+
+public class Professor {
+	//My legs are dangling of the egde, stomach full of pills didn`t work again, I pull a bullet in my head and I`m gone (GoodJob)
+	//Look into the mirror and say goodbye. Climb to the roof to see if u can fly <3 (GoodJob)
+
+	
+	//Must Have for a Professor (To GoodJob remember)... then maybe I`ll be coding through it, cuz it`s look`s easier (GoodJob)
+	//Cadastro, Listagem, Edição e Exclusão de professor.
+	//Um professor deve possuir:
+	//nome
+	//cpf
+	//endereço
+	//e-mail
+	//telefone
+	//Categoria
+	//--Especialista
+	//--Mestre
+	//--Doutor
+	//Disciplinas
+	//--Um professor deve estar associado a pelo menos uma disciplina.
+}
