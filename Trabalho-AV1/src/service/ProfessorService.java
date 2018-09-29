4da1b91f2450521a83c6cca7b579f49ed6712990
Gabriel Julio <Gabriel Julio@DESKTOP-TRH9SIT>
2018-09-29 20:46:56 -0300

    Creating the class ProfessorService
    but getting the error 'constructor is undefined'

--- Trabalho-AV1/src/service/ProfessorService.java
+++ Trabalho-AV1/src/service/ProfessorService.java
@@ -11,7 +11,7 @@ public class ProfessorService {
 		return this.listaProfessores;
 	}
 	
-	//Receita professor
+	//Receita professor - Something is too wrong about this method.
 	public void addProfessor(String nome, long telefone, String cpf, String endereco, String categoria, String disciplina) {
 		Professor p = new Professor(nome, telefone, cpf, endereco, categoria, disciplina);
 		listaProfessores.add(p);
